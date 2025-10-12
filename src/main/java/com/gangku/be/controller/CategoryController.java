ea6b27292ca53ff8ad52afb947245289a6727242
SeriousBug98 <martin1156@konkuk.ac.kr>
2025-10-12 16:12:50 +0900

    fix: URL 매핑 변경

--- src/main/java/com/gangku/be/controller/CategoryController.java
+++ src/main/java/com/gangku/be/controller/CategoryController.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/v1")
 public class CategoryController {
 
     @GetMapping("/categories")
