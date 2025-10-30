8242d731f48bed8f48b7a2ebd65f43ba45468561
seeechan <sechan020830@gmail.com>
2025-10-30 14:00:36 +0900

    feat : Category 관련 요청 허용

--- src/main/java/com/gangku/be/config/SecurityConfig.java
+++ src/main/java/com/gangku/be/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import com.gangku.be.jwt.JwtTokenProvider;
 import com.gangku.be.repository.UserRepository;
 import com.gangku.be.security.JwtAuthFilter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.graphql.GraphQlProperties.Http;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -31,10 +30,12 @@ public class SecurityConfig {
                 .cors(withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(PathRequest.toH2Console()).permitAll()
-                        .requestMatchers(HttpMethod.POST, "/api/*/users").permitAll()
-                        .requestMatchers("/api/*/auth/**").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/api/*/categories/**").permitAll()
+                        .requestMatchers(PathRequest.toH2Console()).permitAll() // H2 콘솔은 인증 없이 허용
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // Preflight 요청 허용
+                        .requestMatchers(HttpMethod.POST, "/api/v1/users").permitAll() //회원가입 허용
+                        .requestMatchers(HttpMethod.POST, "/api/v1/categories").permitAll() //카테고리 생성 허용
+                        .requestMatchers("/api/v1/auth/**").permitAll() //로그인, 토큰 재발급, 로그아웃 등 인증 API 허용
+                        .requestMatchers(HttpMethod.GET, "/api/v1/categories/**").permitAll() //카테고리 조회 인증없이 허용
                         .anyRequest().authenticated()
                 )
                 .headers(headers -> headers.frameOptions(Customizer.withDefaults()).disable());
@@ -44,12 +45,12 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(); // 비밀번호 암호화용 Encoder 등록
     }
 
 
     @Bean
     public JwtAuthFilter jwtAuthFilter() {
-        return new JwtAuthFilter(jwtTokenProvider, userRepository);
+        return new JwtAuthFilter(jwtTokenProvider, userRepository); // JwtAuthFilter 객체 등록
     }
 }
