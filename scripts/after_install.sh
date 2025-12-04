1e01359b463d227af318a5ca8dc3cdc0d6e6ab98
SeriousBug98 <martin1156@konkuk.ac.kr>
2025-12-04 15:54:11 +0900

    fix: 스크립트 수정

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,13 +2,11 @@
 set -euxo pipefail
 cd "$(dirname "$0")/.."
 
-# .env 에서 변수 읽기
-APP_IMAGE="$(grep '^APP_IMAGE=' .env | cut -d= -f2)"
-AWS_REGION="$(grep '^AWS_REGION=' .env | cut -d= -f2)"
+APP_IMAGE=$(grep '^APP_IMAGE=' .env | cut -d= -f2)
+AWS_REGION=$(grep '^AWS_REGION=' .env | cut -d= -f2)
 
-# ECR 로그인
-aws ecr get-login-password --region "$AWS_REGION" \
- | docker login --username AWS --password-stdin "$(echo "$APP_IMAGE" | awk -F: '{print $1}')"
+ECR_REGISTRY=$(echo $APP_IMAGE | cut -d'/' -f1)
 
-# 최신 이미지 풀
-sudo docker pull "$APP_IMAGE"
+aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ECR_REGISTRY
+
+sudo docker pull $APP_IMAGE
