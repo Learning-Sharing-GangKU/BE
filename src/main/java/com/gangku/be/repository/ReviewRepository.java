6a23ac942a18ebe3dac82673fbf58f9a5ef20607
SeriousBug98 <martin1156@konkuk.ac.kr>
2026-03-03 15:10:09 +0900

    test: 리뷰 도메인 및 레포지토리 테스트

---
+++ src/main/java/com/gangku/be/repository/ReviewRepository.java
@@ -0,0 +1,16 @@
+package com.gangku.be.repository;
+
+import com.gangku.be.domain.Review;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    Page<Review> findByRevieweeId(Long revieweeId, Pageable pageable);
+
+    Optional<Review> findByIdAndReviewerId(Long reviewId, Long reviewerId);
+
+    boolean existsByGatheringIdAndReviewerIdAndRevieweeId(Long gatheringId ,Long reviewerId, Long revieweeId);
+}
