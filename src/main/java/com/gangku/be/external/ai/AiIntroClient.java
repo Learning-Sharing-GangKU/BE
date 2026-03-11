37713a3a9caad1f930ecd96356524c2a2a1ea200
SeriousBug98 <martin1156@konkuk.ac.kr>
2026-03-11 17:48:29 +0900

    remove: 필요없는 파일 삭제

--- src/main/java/com/gangku/be/external/ai/AiIntroClient.java
+++
@@ -1,8 +0,0 @@
-package com.gangku.be.external.ai;
-
-import com.gangku.be.dto.gathering.request.GatheringIntroRequestDto;
-import com.gangku.be.dto.gathering.response.GatheringIntroResponseDto;
-
-public interface AiIntroClient {
-    GatheringIntroResponseDto createIntro(GatheringIntroRequestDto request);
-}
--- src/main/java/com/gangku/be/external/ai/util/AiHttpClientUtil.java
+++
@@ -1,5 +0,0 @@
-package com.gangku.be.external.ai.util;
-
-public class AiHttpClientUtil {
-
-}
