0d9e756ed3ef0565546545b78fe3388609ab4d3e
SeriousBug98 <martin1156@konkuk.ac.kr>
2026-03-23 19:33:51 +0900

    rename: 역할에 맞게 클래스명 변경 및 디렉토리 변경

---
+++ src/main/java/com/gangku/be/model/ai/PopularityRefreshResponse.java
@@ -0,0 +1,20 @@
+package com.gangku.be.model.ai;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
+import java.util.Map;
+
+public record PopularityRefreshResponse(
+        @JsonProperty("total_logs")
+        int totalLogs,
+
+        @JsonProperty("n_clusters")
+        int nClusters,
+
+        @JsonProperty("top_n")
+        int topN,
+
+        @JsonProperty("cluster_popularity")
+        Map<Integer, List<Integer>> clusterPopularity
+) {
+}
