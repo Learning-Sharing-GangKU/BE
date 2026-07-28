99dc3c391a5406284407b13c907b4266f7b6f8c2
seeechan <sechan020830@gmail.com>
2026-07-28 15:22:05 +0900

    fix : "모임 카드에서 참여자수 대신 모임날짜로 변경"

--- src/test/java/com/gangku/be/dto/gathering/response/GatheringListResponseDtoSerializationTest.java
+++ src/test/java/com/gangku/be/dto/gathering/response/GatheringListResponseDtoSerializationTest.java
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.RedisSerializer;
 
 /**
